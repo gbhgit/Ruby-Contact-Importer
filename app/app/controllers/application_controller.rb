87b140b5e3aeaa270502916551137de0e4d13d36
gbhgit <gabrielbandeira@lapisco.ifce.edu.br>
2021-09-05 01:36:24 -0300

    [issue-9] organize controllers

--- app/app/controllers/application_controller.rb
+++ app/app/controllers/application_controller.rb
@@ -1,17 +1,17 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  helper_method :current_user, :logged_in?
+  helper_method :current_user, :logged?
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
-  def logged_in?
+  def logged?
     !!current_user
   end
 
   def require_user
-    unless logged_in?
+    unless logged?
       flash[:alert] = 'You must be logged in to perform that action.'
       redirect_to login_path
     end
