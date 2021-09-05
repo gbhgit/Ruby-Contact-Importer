ff2c9b42ad32327575e312976cd0203f894e3c8d
gbhgit <gabrielbandeira@lapisco.ifce.edu.br>
2021-09-05 01:37:19 -0300

    [issue-9] add verification unique email

--- app/app/models/user.rb
+++ app/app/models/user.rb
@@ -4,4 +4,5 @@ class User < ApplicationRecord
   has_secure_password
   has_many :imports
   has_many :contacts
+  validates_uniqueness_of :email
 end
--- app/db/migrate/20210904130444_create_users.rb
+++ app/db/migrate/20210904130444_create_users.rb
@@ -3,7 +3,7 @@
 class CreateUsers < ActiveRecord::Migration[6.1]
   def change
     create_table :users do |t|
-      t.string :email
+      t.string :email, unique: true
       t.string :password_digest
       t.timestamps
     end
